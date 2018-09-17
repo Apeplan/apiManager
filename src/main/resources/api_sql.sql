02699f084592cf6f37a058d3868a81987d607dc5
nba048 <nba048@hotmail.com>
2018-09-17 11:23:45 +0800

    Update api_sql.sql

--- src/main/resources/api_sql.sql
+++ src/main/resources/api_sql.sql
@@ -1,14 +1,61 @@
 /*
 Navicat MySQL Data Transfer
 
+Source Server         : mine
+Source Server Version : 50626
+Source Host           : 192.168.0.29:3306
+Source Database       : api
+
 Target Server Type    : MYSQL
-Target Server Version : 50636
+Target Server Version : 50626
 File Encoding         : 65001
 
-Date: 2018-01-06 13:07:17
+Date: 2018-09-17 11:21:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for bi_bill
+-- ----------------------------
+DROP TABLE IF EXISTS `bi_bill`;
+CREATE TABLE `bi_bill` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_name` int(11) DEFAULT NULL COMMENT '产品名称',
+  `shop_type` int(11) DEFAULT NULL COMMENT '产品型号',
+  `shop_sport` int(11) DEFAULT NULL COMMENT '加工动作',
+  `shop_more` int(11) DEFAULT NULL COMMENT '备注',
+  `money_all` double DEFAULT NULL COMMENT '小计',
+  `number` int(11) DEFAULT NULL COMMENT '数量',
+  `shop_money` double DEFAULT NULL COMMENT '单价',
+  `bi_id` int(11) DEFAULT NULL COMMENT '对应bi_order_id',
+  PRIMARY KEY (`id`),
+  KEY `bi_id` (`bi_id`) USING BTREE,
+  CONSTRAINT `bi_bill_ibfk_1` FOREIGN KEY (`bi_id`) REFERENCES `bi_order` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for bi_order
+-- ----------------------------
+DROP TABLE IF EXISTS `bi_order`;
+CREATE TABLE `bi_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_time` int(11) DEFAULT NULL COMMENT '单据时间',
+  `bill_title` varchar(255) DEFAULT NULL COMMENT '单据标题',
+  `bill_money` double DEFAULT NULL COMMENT '总金额',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for dt_datatype
+-- ----------------------------
+DROP TABLE IF EXISTS `dt_datatype`;
+CREATE TABLE `dt_datatype` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL COMMENT '类型名称',
+  `type` int(11) DEFAULT NULL COMMENT '数据类型:0产品名称,1型号,2加工动作,3备注内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for fo_forder
@@ -20,14 +67,9 @@ CREATE TABLE `fo_forder` (
   `fo_create_time` int(11) DEFAULT NULL COMMENT '创建时间',
   `pr_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `pr_id` (`pr_id`),
+  KEY `pr_id` (`pr_id`) USING BTREE,
   CONSTRAINT `fo_forder_ibfk_1` FOREIGN KEY (`pr_id`) REFERENCES `pr_project` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Records of fo_forder
--- ----------------------------
-INSERT INTO `fo_forder` VALUES ('44', '小明明', '1515132102', '92');
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for hi_histroy
@@ -40,15 +82,11 @@ CREATE TABLE `hi_histroy` (
   `in_id` int(11) DEFAULT NULL,
   `pr_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `hi_histroy_ibfk_2` (`pr_id`),
-  KEY `in_id` (`in_id`),
-  CONSTRAINT `hi_histroy_ibfk_2` FOREIGN KEY (`pr_id`) REFERENCES `pr_project` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `hi_histroy_ibfk_3` FOREIGN KEY (`in_id`) REFERENCES `in_interface` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Records of hi_histroy
--- ----------------------------
+  KEY `hi_histroy_ibfk_2` (`pr_id`) USING BTREE,
+  KEY `in_id` (`in_id`) USING BTREE,
+  CONSTRAINT `hi_histroy_ibfk_1` FOREIGN KEY (`pr_id`) REFERENCES `pr_project` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `hi_histroy_ibfk_2` FOREIGN KEY (`in_id`) REFERENCES `in_interface` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for in_interface
@@ -65,14 +103,9 @@ CREATE TABLE `in_interface` (
   `in_response_ok` longtext COMMENT '成功时的响应内容',
   `in_response_err` longtext COMMENT '失败时的响应内容',
   PRIMARY KEY (`id`),
-  KEY `fo_id` (`fo_id`),
+  KEY `fo_id` (`fo_id`) USING BTREE,
   CONSTRAINT `in_interface_ibfk_1` FOREIGN KEY (`fo_id`) REFERENCES `fo_forder` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Records of in_interface
--- ----------------------------
-INSERT INTO `in_interface` VALUES ('68', '小接口', null, 'POST', null, null, '44', null, null);
+) ENGINE=InnoDB AUTO_INCREMENT=170 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for pa_param
@@ -85,13 +118,9 @@ CREATE TABLE `pa_param` (
   `pa_example` varchar(255) DEFAULT NULL COMMENT '参数示例',
   `in_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `in_id` (`in_id`),
+  KEY `in_id` (`in_id`) USING BTREE,
   CONSTRAINT `pa_param_ibfk_1` FOREIGN KEY (`in_id`) REFERENCES `in_interface` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=285 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Records of pa_param
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=655 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for pr_project
@@ -99,16 +128,11 @@ CREATE TABLE `pa_param` (
 DROP TABLE IF EXISTS `pr_project`;
 CREATE TABLE `pr_project` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pr_name` varchar(255) DEFAULT NULL COMMENT '工程名',
+  `pr_name` varchar(50) DEFAULT NULL COMMENT '工程名',
   `pr_create_time` int(11) DEFAULT NULL COMMENT '创建时间',
-  `pr_root_url` varchar(50) DEFAULT NULL COMMENT '工程主路径',
+  `pr_info` varchar(255) DEFAULT NULL COMMENT '工程主路径',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Records of pr_project
--- ----------------------------
-INSERT INTO `pr_project` VALUES ('92', '测试工程', '1515132088', null);
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for re_response
@@ -120,13 +144,9 @@ CREATE TABLE `re_response` (
   `re_type` int(11) DEFAULT NULL COMMENT '响应类型：0，成功，１失败',
   `in_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `in_id` (`in_id`),
+  KEY `in_id` (`in_id`) USING BTREE,
   CONSTRAINT `re_response_ibfk_1` FOREIGN KEY (`in_id`) REFERENCES `in_interface` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=432 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Records of re_response
--- ----------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for us_user
@@ -143,12 +163,7 @@ CREATE TABLE `us_user` (
   `us_regest_time` int(11) DEFAULT NULL COMMENT '注册时间',
   `us_last_time` int(11) DEFAULT NULL COMMENT '最后登陆时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Records of us_user
--- ----------------------------
-INSERT INTO `us_user` VALUES ('27', null, '7945bd83237335e5376ff44d62e4f0ae', '13800138000', null, '13800138000', null, '1515132050', '1515214958');
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for user_project
@@ -159,13 +174,13 @@ CREATE TABLE `user_project` (
   `us_id` int(11) DEFAULT NULL,
   `pr_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_project_ibfk_1` (`us_id`),
-  KEY `user_project_ibfk_2` (`pr_id`),
+  KEY `user_project_ibfk_1` (`us_id`) USING BTREE,
+  KEY `user_project_ibfk_2` (`pr_id`) USING BTREE,
   CONSTRAINT `user_project_ibfk_1` FOREIGN KEY (`us_id`) REFERENCES `us_user` (`id`) ON DELETE SET NULL,
   CONSTRAINT `user_project_ibfk_2` FOREIGN KEY (`pr_id`) REFERENCES `pr_project` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Records of user_project
--- ----------------------------
-INSERT INTO `user_project` VALUES ('95', '27', '92');
+) ENGINE=InnoDB AUTO_INCREMENT=130 DEFAULT CHARSET=utf8mb4;
+DROP TRIGGER IF EXISTS `trigger_test`;
+DELIMITER ;;
+CREATE TRIGGER `trigger_test` AFTER INSERT ON `us_user` FOR EACH ROW INSERT INTO hi_histroy (hi_info) VALUES ('test_trigger')
+;;
+DELIMITER ;
